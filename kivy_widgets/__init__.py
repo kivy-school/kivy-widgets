6fef04d1a5e605e794c4d3ceaddbb24010404e77
Filipe Marchesini <filipe.marchesini@gmail.com>
2023-11-14 00:23:07 -0300

    ✨ feat: Loading icon unicodes on __init__

--- kivy_widgets/__init__.py
+++ kivy_widgets/__init__.py
@@ -1,3 +1,17 @@
+import os
+
+from kivy.lang import global_idmap
+
 import kivy_widgets.color_definitions
+
+from .icon_definitions import icon_unicodes
+
+__file__ = os.path.abspath(__file__)
+font_path = os.path.join(
+    os.path.dirname(__file__), "fonts", "materialdesignicons-webfont.ttf"
+)
+
+global_idmap["unicode"] = icon_unicodes
+global_idmap["icon_font"] = font_path
 
 __all__ = ["buttons", "dropdown", "icons"]
